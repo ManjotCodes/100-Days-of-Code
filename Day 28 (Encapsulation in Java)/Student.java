361bda02b38cd8b734dc102eca5d86a961d01cfc
Manjot Singh <110255794+ManjotCodes@users.noreply.github.com>
2022-11-09 21:02:22 +0530

    Create Student.java

---
+++ Day 28 (Encapsulation in Java)/Student.java
@@ -0,0 +1,28 @@
+//A Java class which is a fully encapsulated class.  
+//It has a private data member and getter and setter methods.  
+import java.util.*;  
+public class Student{  
+//private data member  
+private String name;  
+//getter method for name  
+public String getName(){  
+return name;  
+}  
+//setter method for name  
+public void setName(String name){  
+this.name=name  
+}  
+}  
+
+
+//A Java class to test the encapsulated class.   
+class Test{  
+public static void main(String[] args){  
+//creating instance of the encapsulated class  
+Student s=new Student();  
+//setting value in the name member  
+s.setName("vijay");  
+//getting value of the name member  
+System.out.println(s.getName());  
+}  
+}  
