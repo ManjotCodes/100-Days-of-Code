dc1a4def29d719328901de02d7d5cd6e7b42fe96
Manjot Singh <110255794+ManjotCodes@users.noreply.github.com>
2022-11-10 22:04:12 +0530

    Create Exception.java

---
+++ Day 29 (Exception Handling in Java)/Exception.java
@@ -0,0 +1,30 @@
+import java.io.*;
+ 
+class Exception {
+    public static void main (String[] args) {
+      int a=5;
+      int b=0;
+        try{
+          System.out.println(a/b);
+        }
+      catch(ArithmeticException e){
+        e.printStackTrace();
+      }
+    }
+}
+
+
+*/
+  public class Main {
+  public static void main(String[] args) {
+    try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[10]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    } finally {
+      System.out.println("The 'try catch' is finished.");
+    }
+  }
+}
+*/
