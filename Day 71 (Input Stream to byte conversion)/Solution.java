1ef8b6b1c4977508e15e58bd09c53f90d1c01ca4
Manjot Singh <110255794+ManjotCodes@users.noreply.github.com>
2023-01-18 20:20:36 +0530

    Create Solution.java

---
+++ Day 71 (Input Stream to byte conversion)/Solution.java
@@ -0,0 +1,28 @@
+import java.io.InputStream;
+import java.util.Arrays;
+import java.io.ByteArrayInputStream;
+
+
+public class Solution {
+
+  public static void main(String args[]) {
+
+    try {
+
+      // create an input stream
+      byte[] input = {1, 2, 3, 4};
+      InputStream stream = new ByteArrayInputStream(input);
+      System.out.println("Input Stream: " + stream);
+
+      // convert the input stream to byte array
+      byte[] array = stream.readAllBytes();
+      System.out.println("Byte Array: " + Arrays.toString(array));
+
+      stream.close();
+    }
+
+    catch (Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
