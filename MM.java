f7509136cf1f871ec668cc644b727c22a9f370c7
Manjot Singh <110255794+ManjotCodes@users.noreply.github.com>
2023-02-15 15:37:57 +0530

    Add files via upload

---
+++ MM.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MM{
+    public static void main(String args[]){
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int b[][]={{9,8,7},{6,5,4},{3,2,1}};
+        int c[][]=new int[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                c[i][j]=0;
+                for(int k=0;k<3;k++){
+                    c[i][j]+=a[i][k]*b[k][j];
+                    System.out.print(c[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
